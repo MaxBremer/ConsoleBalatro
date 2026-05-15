332848eded7ddc18e093c62b99f926795481f229
Max Bremer <m.bremer@rpgglobal.com>
2026-05-15 16:24:06 -0500

    start of new market options manager

---
+++ ConsoleBalatro/Engine/Market/MarketPullManager.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleBalatro.Engine.Pools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBalatro.Engine.Market
+{
+    public static class MarketPullManager
+    {
+        public static Dictionary<BuyItemType, ItemPool> TypeToPoolMappings = new Dictionary<BuyItemType, ItemPool>()
+        {
+            [BuyItemType.TAROT_CARD] = ItemPool.Tarot,
+            [BuyItemType.SPECTRAL_CARD] = ItemPool.Spectral,
+            [BuyItemType.PLANET_CARD] = ItemPool.Planet,
+            [BuyItemType.JOKER] = ItemPool.Joker,
+            [BuyItemType.VOUCHER] = ItemPool.Voucher,
+        };
+
+        public static void DrawMarketItem(BuyItemType itemType, CardZone zoneToDrawTo, bool applyMarketModifiers = false, bool overrideSpaceLimits = false)
+        {
+
+        }
+
+        
+    }
+}

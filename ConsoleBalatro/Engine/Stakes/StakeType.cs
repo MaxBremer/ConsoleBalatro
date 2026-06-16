00f749ce0566970cc2c5359fbdd6eb1fd6b37de2
Max Bremer <m.bremer@rpgglobal.com>
2026-06-16 16:26:34 -0500

    Beginning stake implementation

---
+++ ConsoleBalatro/Engine/Stakes/StakeType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBalatro.Engine.Stakes
+{
+    public enum StakeType
+    {
+        WHITE,
+        RED,
+        GREEN,
+        BLACK,
+        BLUE,
+        PURPLE,
+        ORANGE,
+        GOLD,
+    }
+}
